9a105dc91b6f8c4ce17a4bc94fc6b94705d660d8
Vagif Aliev <you@example.com>
2026-01-29 16:33:16 +0400

    HW 2

---
+++ HW 2/Controllers/TaskItemsController.cs
@@ -0,0 +1,46 @@
+﻿using ASP_NET_07._TaskFlow_Introduction.Models;
+using ASP_NET_07._TaskFlow_Introduction.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_NET_07._TaskFlow_Introduction.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TaskItemsController : ControllerBase
+{
+    private readonly ITaskItemService _taskItemService;
+    public TaskItemsController(ITaskItemService taskItemService)
+    {
+        _taskItemService = taskItemService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> GetAll()
+    {
+        var taskItems = await _taskItemService.GetAllAsync();
+        return Ok(taskItems);
+    }
+
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<TaskItem>> GetById(int id)
+    {
+        var taskItem = await _taskItemService.GetByIdAsync(id);
+        if (taskItem is null)
+            return NotFound($"TaskItem with ID {id} not found");
+        return Ok(taskItem);
+    }
+
+
+    [HttpPost]
+    public async Task<ActionResult<TaskItem>> Create([FromBody] TaskItem taskItem)
+    {
+        if (!ModelState.IsValid) return BadRequest(ModelState);
+        var createdTaskItem = await _taskItemService.CreateAsync(taskItem);
+        return CreatedAtAction(
+            nameof(GetAll),
+            new { id = createdTaskItem.Id },
+            createdTaskItem);
+    }
+
+}
---
+++ HW 2/Services/TaskItemService.cs
@@ -0,0 +1,63 @@
+﻿using ASP_NET_07._TaskFlow_Introduction.Data;
+using ASP_NET_07._TaskFlow_Introduction.Models;
+using ASP_NET_07._TaskFlow_Introduction.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP_NET_07._TaskFlow_Introduction.Services;
+
+public class TaskItemService : ITaskItemService
+{
+    private readonly TaskFlowDbContext _context;
+
+    public TaskItemService(TaskFlowDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<TaskItem> CreateAsync(TaskItem taskItem)
+    {
+        taskItem.CreatedAt = DateTimeOffset.UtcNow;
+        taskItem.UpdatedAt = null;
+
+        _context.TaskItems.Add(taskItem);
+        await _context.SaveChangesAsync();
+
+        return taskItem;
+    }
+
+    public Task<bool> DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<TaskItem>> GetAllAsync()
+    {
+        return await _context
+            .TaskItems
+            .Include(t => t.Project)
+            .ToListAsync();
+           ;
+    }
+
+    public async Task<TaskItem?> GetByIdAsync(int id)
+    {
+        return await _context
+            .TaskItems
+            .Include(t => t.Project)
+            .FirstOrDefaultAsync(t => t.Id == id);
+    }
+
+    public async Task<IEnumerable<TaskItem>> GetByProjectIdAsync(int projectId)
+    {
+        return await _context
+            .TaskItems
+            .Where(t => t.ProjectId == projectId)
+            .Include(t => t.Project)
+            .ToListAsync();
+    }
+
+    public Task<TaskItem?> UpdateAsync(int id, TaskItem taskItem)
+    {
+        throw new NotImplementedException();
+    }
+}
